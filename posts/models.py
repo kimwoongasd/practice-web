f66a3db190f9a109dfa6eab1b4e9f8201c4d98b1
woong <dndvud123@naver.com>
2022-05-20 20:44:05 +0900

    make post_detail.html

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    asd = models.IntegerField
     dt_create = models.DateTimeField(verbose_name="Date Create", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
     
