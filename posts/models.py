568bb187764cf987c612cc843bae00d794363034
woong <dndvud123@naver.com>
2022-05-20 18:38:35 +0900

    make admin and create post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    asd = models.IntegerField
+    dt_create = models.DateTimeField(verbose_name="Date Create", auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.index),
+    # path('', views.post_list),
+    # path('', views.post_create),
+    # path('', views.post_detail),
+    # path('', views.post_update),
+    # path('', views.post_delete),
+]
