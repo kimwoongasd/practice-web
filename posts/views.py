58fe104bb0f11a6d8e3f48f0e6646e65ec94c20c
woong <dndvud123@naver.com>
2022-05-25 21:53:57 +0900

    make update views

--- posts/views.py
+++ posts/views.py
@@ -28,4 +28,16 @@ def post_create(request):
         post_form = PostForm()
         
     return render(request, 'posts/post_form.html', {'form' : post_form})
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
     
+    else:
+        post_form = PostForm(instance=post)
+    
+    return render(request, 'posts/post_form.html', {'form' : post_form})
